ca1fb4c4ff48711f77ac7ce1e4119cc46532d98c
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2021-09-20 10:44:22 +0200

    use sprintf for readability

--- R/functions/tar-sub_chunk.R
+++ R/functions/tar-sub_chunk.R
@@ -1,10 +1,9 @@
 #' sub_chunk
 #' @import knitr
 sub_chunk <- function(code, chunk_name = NULL, fig_width = 11.5, fig_height = 5.75) {
-  sub_chunk_txt <- paste0(
-    "\n```{r ", chunk_name, ", fig.width = ", fig_width, ", fig.height = ", fig_height, ", echo = FALSE}\n",
-    "(", paste0(deparse(function() code), collapse = ""), ")()",
-    "\n```\n\n"
+  sub_chunk_txt <- sprintf(
+    "\n```{r %s, fig.width = %s, fig.height = %s, echo = FALSE}\n(%s)()\n```\n\n",
+    chunk_name, fig_width, fig_height, paste0(deparse(function() code), collapse = "")
   )
   cat(knitr::knit(text = knitr::knit_expand(text = sub_chunk_txt), quiet = TRUE))
 }
