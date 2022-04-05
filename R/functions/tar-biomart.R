60f0bf5ca3c1d7b6222fda2001bb44732e71f5e1
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2022-04-05 15:54:53 +0200

    fix: handle when attributes not found

--- R/functions/tar-biomart.R
+++ R/functions/tar-biomart.R
@@ -5,13 +5,12 @@
 get_biomart_information <- function(
   ensembl_id,
   rna_level = c("ensembl_gene_id", "ensembl_transcript_id"),
-  organism = "hsapiens_gene_ensembl",
+  organism = c("hsapiens_gene_ensembl", "mmusculus_gene_ensembl", "rnorvegicus_gene_ensembl"),
   version,
   build = 38
 ) {
-  # "hsapiens_gene_ensembl"
-  # "mmusculus_gene_ensembl"
-  # "rnorvegicus_gene_ensembl"
+  rna_level <- match.arg(rna_level, c("ensembl_gene_id", "ensembl_transcript_id"))
+  organism <- match.arg(organism, c("hsapiens_gene_ensembl", "mmusculus_gene_ensembl", "rnorvegicus_gene_ensembl"))
   httr::set_config(httr::config(ssl_verifypeer = FALSE))
   get_mart <- quote(biomaRt::useEnsembl(
     biomart = "ensembl",
@@ -47,30 +46,45 @@ get_biomart_information <- function(
     by = rna_level
   ][j = ensembl_version := ensembl_build_version]
 
-  entrez_dt <- data.table::setDT(biomaRt::getBM(
-    attributes = c(rna_level, "entrezgene_id"),
-    filters = rna_level,
-    values = list_unique_gene,
-    mart = mart
-  ))[
-    j = lapply(.SD, format_columns),
-    by = rna_level
-  ]
+  if (!is.null(biomaRt::searchAttributes(mart, pattern = "entrezgene_id"))) {
+    entrez_dt <- data.table::setDT(biomaRt::getBM(
+      attributes = c(rna_level, "entrezgene_id"),
+      filters = rna_level,
+      values = list_unique_gene,
+      mart = mart
+    ))[
+      j = lapply(.SD, format_columns),
+      by = rna_level
+    ]
+  }
 
-  uniprot_dt <- data.table::setDT(biomaRt::getBM(
-    attributes = c(rna_level, "uniprotswissprot"),
-    filters = rna_level,
-    values = list_unique_gene,
-    mart = mart
-  ))[
-    j = lapply(.SD, format_columns),
-    by = rna_level
-  ]
+  if (!is.null(biomaRt::searchAttributes(mart, pattern = "uniprotswissprot"))) {
+    uniprot_dt <- data.table::setDT(biomaRt::getBM(
+      attributes = c(rna_level, "uniprotswissprot"),
+      filters = rna_level,
+      values = list_unique_gene,
+      mart = mart
+    ))[
+      j = lapply(.SD, format_columns),
+      by = rna_level
+    ]
+  }
 
-  merge(
-    x = ensembl_dt,
-    y = merge(x = entrez_dt, y = uniprot_dt, by = rna_level, all = TRUE),
-    by = rna_level,
-    all.x = TRUE
-  )
+  datasets_exists <- sapply(c("entrez_dt", "uniprot_dt"), exists)
+
+  if (all(datasets_exists)) {
+    merge(
+      x = ensembl_dt,
+      y = merge(x = entrez_dt, y = uniprot_dt, by = rna_level, all = TRUE),
+      by = rna_level,
+      all.x = TRUE
+    )
+  } else {
+    merge(
+      x = ensembl_dt,
+      y = get(names(which(datasets_exists))),
+      by = rna_level,
+      all.x = TRUE
+    )
+  }
 }
